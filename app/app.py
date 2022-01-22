50c792bfbf3cec55945598003c2539eb27a21ef5
Adam Simonini <adam.simonini@gmail.com>
2022-01-22 09:08:16 -0500

    got web app to work in docker yaml. moved routes into routes_pkg folder

--- app/app.py
+++ app/app.py
@@ -9,8 +9,8 @@ app.register_blueprint(fsa_blueprint)
 
 @app.route('/')
 def hello_world():
-    return "Default api endpoint"
+    return "Default api endpoint!!! TEST"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8000)
