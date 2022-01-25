1ed10db74edf50ec58ca51e3395989eec84754d4
Adam Simonini <adam.simonini@gmail.com>
2022-01-25 09:25:34 -0500

    updates init.sql with provinces, regions, and hr seed data:

--- data/schema/init.sql
+++ data/schema/init.sql
@@ -17,12 +17,10 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 DROP DATABASE IF EXISTS covid_trends;
---
--- Name: northwind; Type: DATABASE; Schema: -; Owner: postgres
---
+
+-- Name: covid_trends; Type: DATABASE; Schema: -; Owner: postgres
 
 CREATE DATABASE covid_trends WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.utf8';
-
 
 ALTER DATABASE covid_trends OWNER TO postgres;
 
@@ -46,33 +44,75 @@ SET default_table_access_method = heap;
 --
 -- Name: categories; Type: TABLE; Schema: public; Owner: postgres
 --
+
+CREATE TABLE public.regions (
+	id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(35) NOT NULL
+);
+
+-- alphabetically ordered by region name
+INSERT INTO public.regions (name) VALUES
+('Atlantic'), -- 1
+('British Columbia'), -- 2
+('Ontario'), -- 3
+('Prairies'), -- 4
+('Quebec'), -- 5
+('Territories'); -- 6
+
+ALTER TABLE public.regions OWNER TO postgres;
+
 CREATE TABLE public.provinces (
 	province_code INT NOT NULL PRIMARY KEY,
     full_name VARCHAR(35) NOT NULL,
     alpha_code VARCHAR(2) NOT NULL,
-    region VARCHAR(25) NOT NULL
+    region_id VARCHAR(25) NOT NULL
 );
+
+-- alphabetically ordered by province name
+INSERT INTO public.provinces (province_code, full_name, alpha_code, region_id) VALUES
+(48, 'Alberta', 'AB', 4),
+(59, 'British Columbia', 'BC', 2),
+(46, 'Manitoba', 'MB', 4),
+(10, 'Newfoundland and Labrador', 'NL', 1),
+(13, 'New Brunswick', 'NB', 1),
+(61, 'Northwest Territories', 'NT', 6),
+(12, 'Nova Scotia', 'NS', 1),
+(62, 'Nunavut', 'NU', 6),
+(35, 'Ontario', 'ON', 3),
+(11, 'Prince Edward Island', 'PE', 1),
+(24, 'Quebec', 'QC', 5),
+(47, 'Saskatchewan', 'SK', 4),
+(60, 'Yukon', 'AB', 6);
 
 ALTER TABLE public.provinces OWNER TO postgres;
 
 CREATE TABLE public.health_regions (
     hr_uid INT NOT NULL PRIMARY KEY,
-    province_code INT,
-    FOREIGN KEY (province_code) REFERENCES public.provinces (province_code),
+    fk_province_code INT,
+    FOREIGN KEY (fk_province_code) REFERENCES public.provinces (province_code),
     name_en VARCHAR(150) NOT NULL,
     name_fr VARCHAR(150) NOT NULL,
     website_en VARCHAR(300),
     website_fr VARCHAR(300)
 );
 
+-- we have to make sure the website links are to the landing page of the hr website, and not the covid one
+-- doubling up single quotes is the way to escape them
+INSERT INTO public.health_regions (hr_uid, fk_province_code, name_en, name_fr, website_en, website_fr)VALUES
+(1011, 10, 'Eastern Regional Health Authority', 'Eastern Regional Health Authority', 'https://www.gov.nl.ca/covid-19', 'https://www.gov.nl.ca/covid-19/fr/ressources/'),
+(2418, 24, 'Région des Terres-Cries-de-la-Baie-James', 'Région des Terres-Cries-de-la-Baie-James', 'ttps://eeyouistcheecovid19.org/', 'ttps://eeyouistcheecovid19.org/'),
+(3556, 35, 'Porcupine Health Unit', 'Circonscription sanitaire de Porcupin', 'http://www.porcupinehu.on.ca/', 'https://www.porcupinehu.on.ca/fr/'),
+(4603, 46, 'Interlake-Eastern Regional Health Authority', 'Office régional de la santé d''Entre-les-Lacs et de l''Est', 'https://www.ierha.ca/default.aspx?cid=23748', 'https://www.ierha.ca/default.aspx?cid=23748'),
+(594, 59, 'Northern Health', 'Northern Health', 'https://www.northernhealth.ca/health-topics/coronavirus-information-covid-19#trusted-information#northern-health', 'https://www.northernhealth.ca/health-topics/coronavirus-information-covid-19#trusted-information#northern-health');
+
 ALTER TABLE public.health_regions OWNER TO postgres;
 
 CREATE TABLE public.locations(
     forward_sortation_area VARCHAR(3) NOT NULL PRIMARY KEY,
-    hr_uid INT,
-    FOREIGN KEY (hr_uid) REFERENCES public.health_regions (hr_uid),
-	province_code INT,
-    FOREIGN KEY (province_code) REFERENCES public.provinces (province_code)
+    fk_hr_uid INT,
+    FOREIGN KEY (fk_hr_uid) REFERENCES public.health_regions (hr_uid),
+	fk_province_code INT,
+    FOREIGN KEY (fk_province_code) REFERENCES public.provinces (province_code)
 );
 
 ALTER TABLE public.locations OWNER TO postgres;
@@ -131,3 +171,8 @@ CREATE TABLE public.cases(
 );
 
 ALTER TABLE public.cases OWNER TO postgres;
+
+-- TABLE JOIN COMMANDS
+-- SELECT * FROM health_regions hr
+-- LEFT JOIN provinces p
+-- ON p.province_code = hr.fk_province_code;
