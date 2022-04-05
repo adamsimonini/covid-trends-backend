d9e2b7d06cb82ca744f5a1050cdabf933c015d53
Adam Simonini <adam.simonini@gmail.com>
2022-04-05 11:09:41 -0400

    patched start script

--- start-migrate-and-seed-database-with-api-fixtures.sh
+++ start-migrate-and-seed-database-with-api-fixtures.sh
@@ -6,4 +6,4 @@ docker compose exec web python manage.py makemigrations --noinput
 sleep 0
 docker compose exec web python manage.py migrate --noinput
 sleep 0
-docker compose exec web python manage.py loaddata api_fixture
+docker compose exec web python manage.py loaddata disease geo
